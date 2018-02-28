b2b655ac85a2abfd543f7bdfbec05ef54bf25da0
Vishruti Deokar <videokar@cisco.com>
2018-02-27 22:54:20 -0500

    First selemium project

---
+++ rate.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.remitly.com/us/en/india/pricing")
+time.sleep(30)
+
+
+username = driver.find_element_by_id()
+password = driver.find_element_by_id()
+
+
+username.sendkeys()
+password.sendkeys()
+
+login_attempt = driver.find_element_by_xpath()
+
